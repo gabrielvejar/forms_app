67fc6799fd59353dbc8c775573e2ee22cee53ac3
gabrielvejar1 <gabriel.vejar@hfsolutions.cl>
2025-06-09 22:38:59 -0400

    Refactor counter management methods in BlocCounterView to use new increaseBy and resetCounter methods

--- lib/presentation/screens/bloc_counter_screen.dart
+++ lib/presentation/screens/bloc_counter_screen.dart
@@ -15,11 +15,13 @@ class BlocCounterView extends StatelessWidget {
   const BlocCounterView({super.key});
 
   void increaseCounterBy(BuildContext context, [int value = 1]) {
-    context.read<CounterBloc>().add(CounterIncreased(value));
+    // context.read<CounterBloc>().add(CounterIncreased(value));
+    context.read<CounterBloc>().increaseBy(value);
   }
 
   void resetCounter(BuildContext context) {
-    context.read<CounterBloc>().add(CounterReset());
+    // context.read<CounterBloc>().add(CounterReset());
+    context.read<CounterBloc>().resetCounter();
   }
 
   @override
